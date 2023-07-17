97380e9643a2759d9e8470153ca3e9e6620f6386
dhagnere <61789321+dhagnere@users.noreply.github.com>
2023-07-17 21:29:51 +0200

    global commit

---
+++ client/tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        "primary": "#F15412",
+        "secondary": "#1B2430",
+        "third": "#00BFFF",
+        "active": "#057305",
+    }
+  },
+  plugins: [],
+}
+}
